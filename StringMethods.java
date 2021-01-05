e2ae8711b9140c9ca2292e0758a6ad464da8446a
Jatin Ambasana <jatinambasana@gmail.com>
2021-01-05 14:04:28 +0530

    String examples added

---
+++ StringMethods.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class StringMethods {
+    public static void main(String args[])
+    {
+        System.out.print("Enter a String: ");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        System.out.println("Length : " + str.length());
+        System.out.println("Character at location 2: " + str.charAt(2) );
+        System.out.println("Concated with other string:" + str.concat(" abcd"));
+        System.out.println("To upper case: " + str.toUpperCase());
+        System.out.println("To lower case: " + str.toLowerCase()); 
+        System.out.println("Trimmed string: " + str.trim());
+    }
+}
---
+++ StringMethods2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class StringMethods2 {
+    public static void main(String args[])
+    {
+        // String s1 = "Welcome to Java";
+        // String s2 = "Welcome to Java";
+        // String s3 = "Welcome to C++";
+        // String s4 = "Welcome to java";
+
+        // System.out.println("Equals : " + s1.equals(s2));
+        // System.out.println("Equals : " + s1.equals(s3));
+        // System.out.println("EqualsIgnoreCase: " + s1.equalsIgnoreCase(s4));
+       
+        // System.out.println("CompareTo : " + s1.compareTo(s2));
+        // System.out.println("CompareTo : " + s1.compareTo(s4));
+
+        // System.out.println("Starts With Wel: " + s1.startsWith("Wel"));
+        // System.out.println("Starts With wel: " + s1.startsWith("wel"));
+
+
+        String message = "Welcome to Java";
+        String msg = "c++ is Object oriented language";
+        String s1 = message.substring(0, 11) + "HTML"; // Welcome to HTML
+        String s2 = "java" + msg.substring(3);
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
+}
