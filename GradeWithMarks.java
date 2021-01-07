0b799cac67d3e5e77d988f255210e0e81a77bc78
Harshit Dwivedi <77053371+Harshit1109@users.noreply.github.com>
2021-01-07 12:51:42 +0530

    GradeWithMarks.java

---
+++ GradeWithMarks.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class GradeWithMarks{
+	public static void main(String[] args){
+		System.out.println("Enter the marks: ");
+		Scanner sc= new Scanner(System.in);
+		int marks= sc.nextInt();
+		if(marks<=100 && marks >=90){
+			System.out.println("Grade obtained: A");
+		}else if(marks < 90 && marks >=80){
+			System.out.println("Grade obtained: B");
+		}else if(marks < 80 && marks >=70){
+			System.out.println("Grade obtained: C");
+		}else if(marks < 70 && marks >=60){
+			System.out.println("Grade obtained: D");
+		}else 
+		System.out.println("Grade obtained: E");
+	}
+	
+}
