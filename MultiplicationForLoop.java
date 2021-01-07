278e22d2376acd5ef0b494270c31267e6f51cac2
Jatin Ambasana <jatinambasana@gmail.com>
2021-01-07 11:08:00 +0530

    Multiplication example updated

--- MultiplicationForLoop.java
+++ MultiplicationForLoop.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class MultiplicationForLoop {
     public static void main(String args[])
     {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = input.nextInt();
         for(int i = 1; i <= 10 ; i++)
         {
-            System.out.println(2*i);
+            System.out.println(n + " X " + i + "  =  " + (n*i));
         }
     }
 }
---
+++ SumOfNumsTillZero.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfNumsTillZero {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, sum=0;
+        do
+        {
+            System.out.print("\n Enter a number: ");
+            n = input.nextInt();
+            sum = sum + n;
+        } while (n != 0);
+
+        System.out.println("\n Sum of numbers entered: " + sum);        
+    }
+}
