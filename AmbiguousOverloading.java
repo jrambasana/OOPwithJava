182c7f1e290fa740a40e04196a1575c89bd1c61d
Jatin Ambasana <jatinambasana@gmail.com>
2021-01-12 14:09:46 +0530

    Overloading program added

---
+++ AmbiguousOverloading.java
@@ -0,0 +1,40 @@
+
+
+public class AmbiguousOverloading {
+    public static void main(String[] args) {
+        System.out.println(max(1,2));
+    }
+    public static int max(int n1, int n2)
+    {
+        if(n1>n2)
+            return n1;
+        else
+            return n2;
+    }
+    public static double max(double n1, double n2)
+    {
+        if(n1>n2)
+            return n1;
+        else
+            return n2;
+    }
+    public static int max(int n1, int n2, int n3)
+    {
+        if(n1>n2 && n1>n3)
+            return n1;
+        else if(n2>n1 && n2>n3)
+                return n2;
+             else
+                return n3;
+
+    }
+    public static double max(double n1, double n2, double n3)
+    {
+        if(n1>n2 && n1>n3)
+            return n1;
+        else if(n2>n1 && n2>n3)
+                return n2;
+             else
+                return n3;
+    }
+}
