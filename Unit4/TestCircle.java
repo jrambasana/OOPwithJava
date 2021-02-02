91b4b73ff3cf701efb667a89667f124b3f3e7591
Jatin Ambasana <jatinambasana@gmail.com>
2021-02-02 13:59:52 +0530

    Update TestCircle.java

--- Unit4/TestCircle.java
+++ Unit4/TestCircle.java
@@ -1,9 +1,13 @@
 package Unit4;
 
+import java.util.Scanner;
+
 class Circle {
     double radius;
     Circle ()
     {
+        int radius=10;
+        System.out.println(radius);
     }
     Circle (double d)
     {
@@ -23,9 +27,16 @@ public class TestCircle {
     public static void main(String[] args) {
         Circle c1 = new Circle();
         Circle c2 = new Circle(10.5);
+        Circle c3;
         System.out.println("Circle 1 area is: " + c1.getArea());
         c1.setRadius(5);
         System.out.println("Circle 1 area is: " + c1.getArea());
         System.out.println("Circle 2 area is: " + c2.getArea());
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a radius for Circle 3: ");
+        int r = input.nextInt();
+        c3 = new Circle(r);
+        System.out.println("Circle 3 area is: " + c3.getArea());
     }
 }
