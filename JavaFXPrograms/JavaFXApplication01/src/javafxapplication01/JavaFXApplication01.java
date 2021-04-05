dd985c7e104c838b095acd96127033b5fd5a03cf
Jatin Ambasana <jatinambasana@gmail.com>
2021-04-05 10:17:17 +0530

    JavaFXApplication updated

--- JavaFXPrograms/JavaFXApplication01/src/javafxapplication01/JavaFXApplication01.java
+++ JavaFXPrograms/JavaFXApplication01/src/javafxapplication01/JavaFXApplication01.java
@@ -1,76 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javafxapplication01;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
+ * Chapter 14 Exercise 15:
  *
- * @author ADMIN
+ *  (Display a STOP sign)
+ *  Write a program that displays a STOP sign, as shown
+ *  in Figure 14.47b. The octagon is in red and the sign is in white.
+ *  (Hint: Place an octagon and a text in a stack pane.)
+ *
+ * Created by Luiz Arantes Sa on 9/13/14.
  */
 public class JavaFXApplication01 extends Application {
-    
-//    @Override
-//    public void start(Stage primaryStage) {
-//        Button btn = new Button();
-//        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//            
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
-//        
-//        StackPane root = new StackPane();
-//        root.getChildren().add(btn);
-//        
-//        Scene scene = new Scene(root, 300, 250);
-//        
-//        primaryStage.setTitle("Hello World!");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
 
- @Override 
- public void start(Stage primaryStage) 
- {
-  
-  GridPane pane = new GridPane();
-  
-  for (int i = 0; i < 3; i++) 
-  {
-   for (int j = 0; j < 3; j++) 
-   {
-    int n = (int)(Math.random() * 3);
-    if (n == 0)
-     pane.add(new ImageView(new Image("images/x.gif")), j, i);
-    else if (n == 1)
-     pane.add(new ImageView(new Image("images/o.gif")), j, i);
-    else
-     continue;
-   }
-  }
 
-  Scene scene = new Scene(pane, 120, 130);
-  primaryStage.setTitle("Tic-Tac-Toe"); 
-  primaryStage.setScene(scene); 
-  primaryStage.show(); 
- }    
-    public static void main(String[] args) {
-        launch(args);
+    private static double WIDTH = 400;
+    private static double HEIGHT = 400;
+    private static double CENTER_X = WIDTH / 2;
+    private static double CENTER_Y = HEIGHT / 2;
+    private static double RADIUS = WIDTH * 0.4;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        StackPane pane = new StackPane();
+        Polygon polygon = new Polygon();
+        polygon.setFill(Color.RED);
+        polygon.setStroke(Color.RED);
+        ObservableList<Double> points = polygon.getPoints();
+        for (int i = 0; i < 8; i++) {
+            points.add(CENTER_X + RADIUS * Math.cos(Math.toRadians(i * 45 + 45 / 2)));
+            points.add(CENTER_Y - RADIUS * Math.sin(Math.toRadians(i * 45 + 45 / 2)));
+        }
+        polygon.setRotate(0);
+        Text text = new Text("STOP");
+        text.setFont(Font.font(100));
+        text.setFill(Color.WHITE);
+
+        pane.getChildren().addAll(polygon, text);
+        Scene scene = new Scene(pane, WIDTH, HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Fans");
+        primaryStage.show();
     }
-    
+    public static void main(String[] args) {
+
+        Application.launch(args);
+
+    }
+
 }
