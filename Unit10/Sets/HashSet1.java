a41064c3c97915196bc7ceabbac9e34d93c4860e
Jatin Ambasana <jatinambasana@gmail.com>
2021-05-07 09:52:06 +0530

    Update HashSet1.java

--- Unit10/Sets/HashSet1.java
+++ Unit10/Sets/HashSet1.java
@@ -1,19 +1,21 @@
 package Sets;
 
 import java.util.*;  
-class HashSet1{  
+class HashSet1
+{  
  public static void main(String args[]){  
   //Creating HashSet and adding elements  
     HashSet<String> set=new HashSet();  
-           set.add("One");    
-           set.add("Two");    
-           set.add("Three");   
-           set.add("Four");  
-           set.add("Five");  
-           Iterator<String> i=set.iterator();  
-           while(i.hasNext())  
-           {  
-           System.out.println(i.next());  
-           }  
+    set.add("One");    
+    set.add("Two");    
+    set.add("Three");   
+    set.add("Four");  
+    set.add("Five");  
+    set.add("One"); 
+    Iterator<String> i=set.iterator();  
+    while(i.hasNext())  
+    {  
+      System.out.println(i.next());  
+    }  
  }  
 }  
